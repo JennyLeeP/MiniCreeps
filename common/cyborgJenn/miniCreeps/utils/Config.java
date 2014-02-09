d94df1c5d23f43fc67e97b611834c216090abdad
JennyLeeP <jennyleep@comcast.net>
2014-02-09 16:10:00 -0500

    Third test

---
+++ common/cyborgJenn/miniCreeps/utils/Config.java
@@ -0,0 +1,34 @@
+package cyborgJenn.miniCreeps.utils;
+
+import java.io.File;
+
+import net.minecraftforge.common.config.Configuration;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class Config {
+	public static Boolean enableDebug;
+	
+	public static void init(FMLPreInitializationEvent event) {
+
+        File configFile = new File(event.getModConfigurationDirectory(), Reference.NAME + ".cfg");
+
+        Configuration config = new Configuration(configFile);
+
+        try{
+            config.load();
+
+            configDebug(config);
+
+        }catch(Exception e){
+            System.out.println(e);
+        }finally{
+            if (config.hasChanged())
+                config.save();
+        }
+    }
+	private static void configDebug(Configuration config){
+        String debug = "Debug";
+        enableDebug = config.get(debug, "enableDebug", false).getBoolean(false);
+      
+    }
+}
--- common/cyborgJenn/miniCreeps/utils/Reference.java
+++ common/cyborgJenn/miniCreeps/utils/Reference.java
@@ -3,5 +3,6 @@ package cyborgJenn.miniCreeps.utils;
 public class Reference {
 	
 	public static final String MODID = "MiniCreeps";
+	public static final String NAME = "MiniCreeps";
     public static final String VERSION = "@VERSION@";
 }
